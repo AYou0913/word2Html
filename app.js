c2462218d1951bbe3364b5a278e28a36314cc63f
Jinyou Liang <jinyouliang@JinyoudeMacBook-Pro.local>
2020-06-01 03:00:29 +0800

    get & post

--- app.js
+++ app.js
@@ -24,6 +24,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/process_get', function(req, res) {
+
+    // 输出 JSON 格式
+    var response = {
+        "first_name": req.query.first_name,
+        "last_name": req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+app.post('/process_post', function(req, res) {
+
+    // 输出 JSON 格式
+    var response = {
+        "first_name": req.body.first_name,
+        "last_name": req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
